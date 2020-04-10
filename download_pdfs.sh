5339ddda990429e536bef10b27e082049243d144
Michael Hutchinson <hutchinson.michael.john@gmail.com>
2020-04-10 16:00:36 +0100

    New data

--- download_pdfs.sh
+++ download_pdfs.sh
@@ -1,4 +1,4 @@
-date="2020-03-29"
+. ./config.sh
 # states="Alabama Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada New_Hampshire New_Jersey New_Mexico New_York North_Carolina North_Dakota Ohio Oklahoma Oregon Pennsylvania Rhode_Island South_Carolina South_Dakota Tennessee Texas Utah Vermont Virginia Washington West_Virginia Wisconsin Wyoming"
 # countries="US GB IT DE ES FR"
 
--- make_data.sh
+++ make_data.sh
@@ -1,11 +1,11 @@
-date="2020-03-29"
+. ./config.sh
 source venv/bin/activate
 
 cat states.txt | while read state ; do
     echo ${state}
     # python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${state} --aggregate-only
     # python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${state} --no-aggregate
-    python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${state}
+    python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${state} --date ${date}
 done
 
 # rename the coutntries pdfs slightly to conform to similar naming convention as the individual states
@@ -13,9 +13,9 @@ cat countries.txt | while read country ; do
     echo ${country}
     # python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${country} --aggregate-only
     # python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${country} --no-aggregate
-    python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${country}
+    python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --place-codes ${country} --date ${date}
 done
 
 # python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --aggregate-only
 # python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --no-aggregate
-python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date}
+python parse_google_pdfs.py --pdfdir pdfs/${date} --outdir data/${date} --date ${date}
